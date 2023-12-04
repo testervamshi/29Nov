9f04f0c4ce0f27240806d644e4eca198fe7aec38
myproject <vamshinani9000@gmail.com>
2023-12-04 16:48:01 +0530

    my commit

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -20,7 +20,7 @@ import org.testng.annotations.Parameters;
 
 public class BaseClass {
 	
-	public static WebDriver driver;
+	public  WebDriver driver;
 	public Logger logger;// for logging
 	public ResourceBundle rb;
 	@BeforeClass
